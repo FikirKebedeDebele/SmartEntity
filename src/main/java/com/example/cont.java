17a3fc979611f1d4baa09914299430493cfd7835
fikireyobed@gmail.com <fikireyobed@gmail.com>
2017-01-14 23:04:25 -0600

    all

---
+++ src/main/java/com/example/cont.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class cont {
+
+	@RequestMapping("/ff")
+	public @ResponseBody User g()
+	{
+		return new User("Fikir","Stable");
+	}
+}
+
+
+class User
+{
+	String name;
+	String status;
+	public User(String name, String status) {
+		super();
+		this.name = name;
+		this.status = status;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
